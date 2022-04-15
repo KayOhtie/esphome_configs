b4c17d191ae200694c6f6d1a55c27199b89756b5
Kay Ohtie <kay@coyotesin.space>
2022-04-15 10:13:58 -0500

    basic script to take HTML colors and convert to colors for animated scenes on prism

---
+++ prism_hex_converter.py
@@ -0,0 +1,17 @@
+import sys
+from textwrap import wrap
+from colorsys import rgb_to_hsv
+colors = sys.argv[1:]
+outstr = ""
+try:
+    for color in colors:
+        rgb = map(lambda x:int(x,16)/255.0, wrap(color,2))
+        hsv = rgb_to_hsv(*rgb)
+        h = int(hsv[0] * 360)
+        s = int(hsv[1] * 1000)
+        v = int(hsv[2] * 1000)
+        hsvb = "%0.4x%0.4x%0.4x00000000" % (h,s,v)
+        outstr += hsvb
+except Exception as e:
+    print(e)
+print(outstr)
